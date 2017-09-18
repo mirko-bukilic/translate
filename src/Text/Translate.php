10c2eeb7c65ace599b29cd9e5b3a6173ac09c11a
Petar Kapulica <petarkapulica@hotmail.com>
2017-09-18 12:27:22 +0200

    Added .po files concatenation to single translation.po file

--- src/Text/Translate.php
+++ src/Text/Translate.php
@@ -16,7 +16,27 @@ class Translate
     public function __construct($localePath)
     {
         $this->localePath = realpath($localePath);
+        $this->concatenatePoFiles();
         $this->iterateTroughLocale();
+    }
+
+    /**
+     * @param array $poFiles
+     */
+    private function concatenate(array $poFiles)
+    {
+        $filesToConcatenate = "";
+        foreach($poFiles as $poFile){
+            $path = $poFile->getPath();
+            $filesToConcatenate .= realpath($poFile->getPath()) . DIRECTORY_SEPARATOR . $poFile->getBasename() . " ";
+        }
+        (new Cmd([
+                'msgcat',
+                '-o',
+                realpath($path) . DIRECTORY_SEPARATOR . 'translation.po',
+                $filesToConcatenate
+            ]
+        ))->execute();
     }
 
     /**
@@ -32,10 +52,25 @@ class Translate
         ]))->execute();
     }
 
+    private function concatenatePoFiles()
+    {
+        $previousPoFiles = [];
+        foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->localePath), \RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
+            if($file->isFile() && $file->getExtension() == 'po'){
+                isset($previousPoFiles[$file->getPath()])
+                    ? $previousPoFiles[$file->getPath()][] = $file
+                    : $previousPoFiles[$file->getPath()] = [$file];
+            }
+        }
+        foreach ($previousPoFiles as $poFiles){
+            $this->concatenate($poFiles);
+        }
+    }
+
     private function iterateTroughLocale()
     {
         foreach (new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($this->localePath), \RecursiveIteratorIterator::LEAVES_ONLY) as $file) {
-            $file->isFile() && $file->getExtension() == 'po' && $this->convert($file);
+            $file->isFile() && $file->getBasename() == 'translation.po' && $this->convert($file);
         }
     }
 }
