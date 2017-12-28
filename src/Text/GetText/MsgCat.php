ed6ed10f62d3c4f3676fd45347007ac5e0f43530
Drasko Gomboc <drasko.gomboc@gmail.com>
2017-12-28 16:14:53 +0100

    MsgCat command

--- src/Text/GetText/MsgCat.php
+++ src/Text/GetText/MsgCat.php
@@ -3,13 +3,60 @@
 
 namespace G4\Translate\Text\GetText;
 
+use G4\ValueObject\RelativePath;
 
 class MsgCat
 {
 
-    public function __construct()
+    const COMMAND_NAME          = 'msgcat';
+
+    const OPTION_USE_FIRST      = '--use-first';
+    const OPTION_FILES_FROM     = '-f';
+    const OPTION_OUTPUT_FILE    = '-o';
+
+    /**
+     * @var RelativePath
+     */
+    private $filesFrom;
+
+    /**
+     * @var RelativePath
+     */
+    private $outputFile;
+
+    /**
+     * @var bool
+     */
+    private $shouldUseFirst;
+
+    /**
+     * MsgCat constructor.
+     * @param array $filesFrom
+     * @param RelativePath $outputFile
+     */
+    public function __construct(array $filesFrom, RelativePath $outputFile)
     {
+        $this->filesFrom        = $filesFrom;
+        $this->outputFile       = $outputFile;
+        $this->shouldUseFirst   = false;
     }
 
+    public function useFirst()
+    {
+        $this->shouldUseFirst = true;
+    }
 
+    /**
+     * @return array
+     */
+    public function format()
+    {
+        return array_filter([
+            self::COMMAND_NAME,
+            $this->shouldUseFirst ? self::OPTION_USE_FIRST : false,
+            self::OPTION_OUTPUT_FILE,
+            (string) $this->outputFile,
+            join(' ', $this->filesFrom),
+        ]);
+    }
 }
---
+++ tests/unit/src/MsgCatTest.php
@@ -0,0 +1,58 @@
+<?php
+
+
+use G4\Translate\Text\GetText\MsgCat;
+use G4\ValueObject\RelativePath;
+
+class MsgCatTest extends PHPUnit_Framework_TestCase
+{
+
+    private $filesFromMock;
+
+    /**
+     * @var MsgCat
+     */
+    private $msgCat;
+
+    private $outputFileMock;
+
+    public function testFormat()
+    {
+        $data = $this->msgCat->format();
+        $this->assertTrue(in_array('msgcat', $data));
+        $this->assertTrue(in_array('-o', $data));
+        $this->assertTrue(in_array('translation.po', $data));
+        $this->assertTrue(in_array('file1.po file2.po', $data));
+    }
+
+    public function testFormatWithUseFirst()
+    {
+        $this->msgCat->useFirst();
+        $data = $this->msgCat->format();
+        $this->assertTrue(in_array('msgcat', $data));
+        $this->assertTrue(in_array('--use-first', $data));
+        $this->assertTrue(in_array('-o', $data));
+        $this->assertTrue(in_array('translation.po', $data));
+        $this->assertTrue(in_array('file1.po file2.po', $data));
+    }
+
+    protected function setUp()
+    {
+        $this->filesFromMock = [
+            'file1.po',
+            'file2.po',
+        ];
+        $this->outputFileMock = $this->getMockBuilder(RelativePath::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $this->outputFileMock->expects($this->once())->method('__toString')->willReturn('translation.po');
+        $this->msgCat = new MsgCat($this->filesFromMock, $this->outputFileMock);
+    }
+
+    protected function tearDown()
+    {
+        $this->filesFromMock    = null;
+        $this->outputFileMock   = null;
+        $this->msgCat           = null;
+    }
+}
